a2768434ee16bd700806116547dd089160bfd508
todah86 <todahlael@gmail.com>
2019-04-19 10:57:04 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,9 +2,8 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-})
+
+  console.log("Hello World");
 
 // --> 7)  Mount the Logger middleware here
 
